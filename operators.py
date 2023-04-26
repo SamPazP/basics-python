bfb3dbd76c784159206497bbb2f84d364e379530
SamPazP <you@example.com>
2023-04-25 22:07:43 -0600

    ADD: Operators.

---
+++ operators.py
@@ -0,0 +1,20 @@
+adition = 10 + 5 # Esto dara como resultado 15
+abstraction = 10 - 5 # Esto dara como resultado 5
+multiplication = 10 * 5 # Esto dara como resultado 50
+
+
+# Comparation operators
+age = 10 == 5 # Dara como resultado False
+age = 10 != 5 # Dara como resultado True
+age = 10 > 5 # Dara como resultado True
+
+# Logicos
+age_boy = 10
+age_girl = 14
+age_boy > 8 and age_girl > 8 # True
+
+# Assigments operators
+age = 10 # Dara 10
+age += 5 # Dara 15
+age -= 5 # Dara 10
+age *= 5 # Dara 50
